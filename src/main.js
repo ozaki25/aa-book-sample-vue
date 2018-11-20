7efcc3e81acb22bc582b81852606f4d653c0159a
ozaki <fs65ugumori@yahoo.co.jp>
2018-11-21 07:47:47 +0900

    add prettierrc

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount("#app");
