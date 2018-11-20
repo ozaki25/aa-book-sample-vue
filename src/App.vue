ff695d3764c40ea2e8d1793a5fd6433ba44b4af4
ozaki <fs65ugumori@yahoo.co.jp>
2018-11-21 07:58:35 +0900

    add sample table component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <PersonsTable />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue';
+import PersonsTable from './components/persons/PersonsTable';
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    PersonsTable,
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/persons/PersonsTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <table class="persons-table table table-bordered table-striped">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>名前</th>
+        <th>年齢</th>
+        <th>性別</th>
+        <th></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>ozaki</td>
+        <td>28</td>
+        <td>男性</td>
+        <td><button class="btn btn-xs btn-default">編集</button></td>
+        <td><button class="btn btn-xs btn-default">削除</button></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'PersonsTable',
+  props: {},
+};
+</script>
