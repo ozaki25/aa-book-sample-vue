b4ac643fd63ac9d9696d75f892062646468b3ebb
ozaki <fs65ugumori@yahoo.co.jp>
2018-11-22 07:47:46 +0900

    add form component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <div id="app" class="container">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <PersonsTable />
+  <div  class="container">
+    <div id="app">
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">Vue Sample</div>
+      <div class="panel-body">
+        <PersonsForm />
+      </div>
+      <PersonsTable />
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 import PersonsTable from './components/persons/PersonsTable';
+import PersonsForm from './components/persons/PersonsForm';
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
     PersonsTable,
+    PersonsForm
   },
 };
 </script>
---
+++ src/components/persons/PersonsForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <form class="form-inline">
+    <div class="form-group">
+      <label for="input-id" class="label-control">ID</label>
+      <input id="input-id" class="form-control" readonly />
+    </div>
+    <div class="form-group">
+      <label for="input-name" class="label-control">名前</label>
+      <input id="input-name" class="form-control" />
+    </div>
+    <div class="form-group">
+      <label for="input-age" class="label-control">年齢</label>
+      <input id="input-age" class="form-control" type="number" />
+    </div>
+    <div class="form-group">
+      <label for="input-gender" class="label-control">性別</label>
+      <select id="input-gender" class="form-control">
+        <option value="" />
+        <option value="男性">男性</option>
+        <option value="女性">女性</option>
+      </select>
+    </div>
+    <button id="button-register" type="button" class="btn btn-default">
+      登録
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'PersonsForm',
+  props: {},
+};
+</script>
